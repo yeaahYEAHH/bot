0f150730c99494cf954d74960e4fc9d334684125
yeaahYEAHH <81806907+yeaahYEAHH@users.noreply.github.com>
2022-10-07 06:55:32 +0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,11 @@
+let token = 'vk1.a.Raoal_4MrkjiavRBk_hnF63AfffVA4inurn7rggGqmtGQR2gmoWE6pt13vEO8WuQJWuCb3Nm3zImJ0HFxKWXKNX_S9wCcC5XE20MAvWoNQpuoPWjKLvgI-hgaOtVZbci1an-17zboWkMoITOc9moEERd5ddHzt6WNSyPqhq-5eUOOW_qvoDkA_xLo_OQzxIV';
+
+const VkBot = require('node-vk-bot-api');
+
+const bot = new VkBot(token);
+
+bot.on((ctx) => {
+  ctx.reply('Hello!');
+});
+
+bot.startPolling();
